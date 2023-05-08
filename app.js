6155d401c117c5c201defeade4ff315a7eeb54ab
김하영 <khy4018@gmail.com>
2023-05-08 18:26:21 +0900

    :tada: init project

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const config = require('./config')
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+mongoose.connect(config.mongodbUri)
+const db = mongoose.connection
+db.on('error', console.error)
+db.once('open', ()=>{
+    console.log('connected to mongodb server')
+    app.listen(3000)
+})
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.use(authRoutes);
